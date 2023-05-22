418380ba4760e615e152efaf894a3832db23d0b1
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-05-23 01:13:36 +0300

    v1

---
+++ api_yamdb/users/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import CustomUser
+import re
+
+
+class ConfirmationSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    email = serializers.CharField(max_length=254)
+
+    def validate_username(self, value):
+        if value == 'me':
+            raise serializers.ValidationError('Нельзя использовать "me"')
+        pattern = re.compile(r'^[\w.@+-]+\Z')
+        if not pattern.match(value):
+            raise serializers.ValidationError('Ошибка паттерна')
+        return value
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'username', 'bio', 'email',
+                  'role']
+        lookup_field = 'username'
+
+
+class CustomUserPATCHSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'username', 'bio', 'email', ]
+        lookup_field = 'username'
+
+
+class ConfirmationCodeSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    confirmation_code = serializers.CharField()
---
+++ api_yamdb/users/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes, action
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from rest_framework.response import Response
+from rest_framework import status, permissions, viewsets, filters
+from rest_framework_simplejwt.tokens import AccessToken
+from api.permissions import IsAdminCustomUser
+from api_yamdb.settings import DEFAULT_FROM_EMAIL
+
+from .models import CustomUser
+from .serializers import (ConfirmationSerializer, CustomUserSerializer,
+                          CustomUserPATCHSerializer,
+                          ConfirmationCodeSerializer)
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny, ))
+def get_confirmation_code(request):
+    serializer = ConfirmationSerializer(data=request.data)
+    if not serializer.is_valid():
+        # тут подстроили вывод под pytest
+        return Response(
+            {
+                "email": ["некорректно заполнено"],
+                "username": ["некорректно заполнено"]
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    try:
+        user = CustomUser.objects.get_or_create(
+            username=request.data['username'],
+            email=request.data['email']
+        )
+    except Exception as error:
+        return Response(
+            {'Ошибка': str(error)},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    confirmation_code = default_token_generator.make_token(user[0])
+    send_mail(
+        'Confirmation code for YaMDb',
+        f'Your confirmation code: {confirmation_code}',
+        f'{DEFAULT_FROM_EMAIL}',
+        [f'{request.data["email"]}'],
+        fail_silently=False,
+    )
+    return Response(request.data, status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def get_token(request):
+    serializer = ConfirmationCodeSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    username = request.data.get('username')
+    user = get_object_or_404(CustomUser, username=username)
+    code = request.data.get('confirmation_code')
+    if default_token_generator.check_token(user, code):
+        user.save()
+        token = AccessToken.for_user(user)
+        return Response(
+            {'message': f'Your token is {token}'}, status.HTTP_200_OK)
+    return Response({"message": "неверный код подтверждения."},
+                    status.HTTP_400_BAD_REQUEST)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminCustomUser,)
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ['username', ]
+    lookup_field = 'username'
+    http_method_names = ['get', 'post', 'delete', 'patch']
+
+    @action(methods=['GET', 'PATCH'], detail=False,
+            permission_classes=[permissions.IsAuthenticated])
+    def me(self, request):
+        if request.method == 'GET':
+            serializer = CustomUserSerializer(self.request.user)
+        else:
+            serializer = CustomUserPATCHSerializer(
+                self.request.user, data=request.data, partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        return Response(serializer.data)
